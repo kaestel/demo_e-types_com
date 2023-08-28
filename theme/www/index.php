d540fed6363acd78639b5f2bc9ebb9e1d58cda9c
kaestel <martin@think.dk>
2023-08-28 14:26:04 +0200

    updated

---
+++ theme/www/index.php
@@ -0,0 +1,289 @@
+<?php include_once($_SERVER["LOCAL_PATH"]."/includes/segment.php") ?>
+<!DOCTYPE html>
+<html lang="EN">
+<head>
+	<!-- (c) & (p) e-types.com, 2014 //-->
+	<!-- All material protected by copyrightlaws, as if you didnt know //-->
+	<title>e-Types.com</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="keywords" content="" />
+	<meta name="description" content="We challenge your ways and encourage new thinking. We craft bold solutions that inspire people around us. We don’t follow trends but work hard to make others follow you. We crystallize big ideas to build strong brands. Brands that stand out and clearly communicate what they are all about. Brands that move the world forward. We are e-types, e-Types Daily and Playtype." />
+	<meta name="viewport" content="initial-scale=1, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<link type="text/css" rel="stylesheet" media="all" href="/css/seg_<?= $_SESSION["segment"] ?>.css" />
+	<script type="text/javascript" src="/js/seg_<?= $_SESSION["segment"] ?>.js"></script>
+</head>
+
+<body class="front">
+
+<div id="page" class="i:page">
+	<div id="header">
+		<ul class="servicenavigation">
+			<li class="keynav navigation nofollow"><a href="#navigation">To navigation</a></li>
+			<li class="contact"><a href="/contact">Contact</a></li>
+			<li class="welcome"><a href="/">Welcome</a></li>
+		</ul>
+	</div>
+
+	<div id="content">
+
+		<div class="scene front i:front gridtype:combined">
+			<h1>And you are?</h1>
+			<h2>
+				We are brand and design agencies <span class="name">e-Types</span> and <span class="name">e-Types</span> Daily, and type foundry and concept store Playtype.
+				We create meaningful and attractive brands based on strong ideas and strategy. Welcome.
+			</h2>
+
+			<ul class="news">
+
+				<li class="news item_id:338 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:what_do_you_want" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/what_do_you_want">What Do You Want?</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2015-04-08 15:52:58</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Roskilde University</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>e-Types has created the ‘Hvad vil du?’ (What Do You Want?) recruitment campaign for Roskilde University. This year, the campaign portrays seven current bacherlor students, telling about their educational programs and their project work at RUC.</p>
+					</div>
+				</li>
+
+				<li class="news item_id:336 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:brand_new_sustainability_website_for_rockwool" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/brand_new_sustainability_website_for_rockwool">Brand New Sustainability Website for ROCKWOOL</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2015-04-07 10:42:47</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">ROCKWOOL</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>e-Types has developed and designed a brand new sustainability website for The ROCKWOOL Group. </p>
+					</div>
+				</li>
+
+				<li class="news item_id:208 i_po:jpg i_la:jpg i_sq:jpg v_po:mov v_la:mov v_sq:mov url:effortless_style" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/effortless_style">Effortless Style</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="creator">e-Types</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Soulland</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">Dec 03, 2013</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>The new soulland.com is an ambitious take on a webshop that combines experiences and sale – two ingredients that rarely go hand in hand, and that few have succeeded in balancing. e-Types and Soulland took up the challenge.</p>
+					</div>
+				</li>		
+
+				<li class="news item_id:331 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:branding_and_packaging_for_the_new_brewm" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/branding_and_packaging_for_the_new_brewm">Branding and packaging for the new Brewmasters’ Collection</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2015-03-11 12:19:50</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Carlsberg</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>Together with Carlsberg’s innovation department, e-Types has developed the brand story and visual identity for a new range of ‘local craft’ beers.</p>
+					</div>
+				</li>
+
+				<li class="news item_id:329 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:visual_identity_for_greater_copenhagen" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/visual_identity_for_greater_copenhagen">Visual identity for Greater Copenhagen</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2015-03-10 15:07:58</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Region Hovedstaden</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>e-Types has designed the visual identity for Greater Copenhagen - a collaboration between a number of Danish and South Swedish municipalities</p>
+					</div>
+				</li>
+
+				<li class="news item_id:49 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:for_the_love_of_typography" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/for_the_love_of_typography">For the Love of Typography</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="creator">Playtype</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">May 15, 2013</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Beynderground</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>Playtype attended the graphic design festival Beynderground in Hasselt, Belgium.</p>
+					</div>
+				</li>
+
+				<li class="news item_id:313 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:campaign_for_b_o" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/campaign_for_b_o">Campaign for B&O</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2015-01-30 11:25:00</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Bang & Olufsen</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>Campaign for Bang & Olufsen's intelligent music system BeoSound Moment.</p>
+					</div>
+				</li>
+
+				<li class="news item_id:304 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:tidal_sounds_perfect_" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/tidal_sounds_perfect_">TIDAL - Sounds. Perfect.</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2014-10-29 10:38:13</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">TIDAL</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>We’ve created the visual identity, the concept and the brand launch campaign for Wimp’s new and innovative music streaming concept, TIDAL.</p>
+					</div>
+				</li>
+
+				<li class="news item_id:302 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:new_commissioned_typeface_for_skagen_den" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/new_commissioned_typeface_for_skagen_den">New commissioned typeface for Skagen Denmark</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">Playtype</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2014-10-01 14:00:10</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Skagen</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>Playtype’s and e-Types’ Jens Kajus and Jonas Hecksher recently designed a bespoke typeface for Skagen Denmark, where the landscape of Skagen became a source of inspiration.</p>
+					</div>
+				</li>
+
+				<li class="news item_id:204 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:less_is_more" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/less_is_more">Less is More</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="creator">e-Types Daily</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">Apr 01, 2013</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Metroselskabet</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>The Metro Annual report is simple, delicate and true to the brand. Details in the finish and on materials compliment the design and lift the overall feel of the report.</p>
+					</div>
+				</li>
+
+				<li class="news item_id:300 i_po:png i_la:png i_sq:png v_po:mov v_la:mov v_sq:mov url:let_s_rethink" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/let_s_rethink">Let’s rethink</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2014-08-26 09:24:20</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">Aarhus 2017</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>What happens when art and culture meet the challenges of tomorrow?</p>
+					</div>
+				</li>
+
+				<li class="news item_id:296 i_po:jpg i_la:jpg i_sq:jpg v_po: v_la: v_sq: url:mood_boards_and_breadcrumbs" itemscope itemtype="http://schema.org/Article">
+					<h2 itemprop="headline"><a href="/article/mood_boards_and_breadcrumbs">Mood boards and breadcrumbs</a></h2>
+					<dl class="info">
+						<dt class="unit">Business unit</dt>
+						<dd class="unit" itemprop="author">e-Types</dd>
+						<dt class="published_at">Published at</dt>
+						<dd class="published_at" itemprop="datePublished">2014-08-06 12:59:23</dd>
+						<dt class="client">Client or category</dt>
+						<dd class="client" itemprop="genre">OeO</dd>
+						<dt class="type">Type</dt>
+						<dd class="type">news</dd>
+					</dl>
+					<div class="article" itemprop="description">
+						<p>Inspired by their unique approach to design, OeO's new website unfolds as a mood board with an endless page carousel with individual scrolls, color changes and visual breadcrumbs.</p>
+					</div>
+				</li>
+
+			</ul>
+		</div>
+	</div>
+
+	<div id="navigation">
+		<ul>
+			<li class="etypes">
+				<h3><a>e-Types</a></h3>
+
+				<div class="submenu">
+					<p>e-Types is a brand agency that crystallizes<br>big ideas to build strong brands.</p>
+					<ul class="subjects">
+						<li class="curated"><a href="/journal/e-types">Journal</a></li>
+						<li class="about"><a href="/about">About</a></li>
+						<li class="competences"><a href="/competences">Competences</a></li>
+					</ul>
+				</div>
+
+			</li>
+			<li class="daily">
+				<h3><a>e-Types Daily</a></h3>
+
+				<div class="submenu">
+					<p>e-Types Daily is a design agency that brings<br>brands to life through day-to-day graphic design.</p>
+					<ul class="subjects">
+						<li class="curated"><a href="/journal/daily">Journal</a></li>
+						<li class="about"><a href="/daily/about">About</a></li>
+						<li class="competences"><a href="/daily/services">Services</a></li>
+					</ul>
+				</div>
+
+			</li>
+			<li class="playtype">
+				<h3><a href="/playtype">Playtype</a></h3>
+			</li>
+		</ul>
+	</div>
+
+
+	<div id="footer">
+		<p class="typedin">Typed in Nationale Regular &amp; DemiBold – available from <a href="http://playtype.com/font/nationale" target="_blank">Playtype</a></p>
+		<p class="photos">All photos on e-types.com is used for case presentation only</p>
+		<div class="sharing"><img src="/img/sharing.jpg" alt="e-Types" /></div>
+	</div>
+</div>
+
+</body>
+</html>
